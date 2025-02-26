67c7ebbb81b5b3e8193ff25ba8211bdde6051439
Johnmahith <122510766+Johnmahith@users.noreply.github.com>
2025-02-26 16:29:44 -0600

    Delete scripts.js

--- scripts.js
+++
@@ -1,5 +0,0 @@
-// Example for handling form submission or simple interactivity
-document.querySelector('form').addEventListener('submit', function(e) {
-    e.preventDefault();
-    alert('Your message has been sent!');
-});
